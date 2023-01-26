2e4e951d7c0993ecd5103816cc85dc6cc196f01c
jot6000 <joemercer50@googlemail.com>
2023-01-26 12:38:38 +0000

    forum-example-frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [posts,setposts] = useState([])
+  const [title,settitle] = useState("")
+  const [content,setcontent] = useState("")
+  useEffect(()=>{
+    fetch("http://localhost:4000/testdata")
+    .then(response => response.json())
+    .then(data => setposts(data))
+  },[])
+
+  const sendPost =(e)=>{
+    e.preventDefault();
+    axios.post('http://localhost:4000/posts',
+      {
+        author: "James",
+        title: title,
+        posted: "01/01/2023",
+        likes: 0,
+        dislikes: 0,
+        content: content,
+        replies: []
+      }
+    ).then( data =>{
+      console.log(data)
+      setcontent("")
+      settitle("")
+      fetch("http://localhost:4000/testdata")
+      .then(response => response.json())
+      .then(data => setposts(data))
+    }
+
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e)=>sendPost(e)}>
+        <h1>Create a post</h1>
+        <label>
+          Title: 
+          <input 
+            type="text" 
+            value={title} 
+            onChange={(e)=>settitle(e.target.value)}
+          />
+        </label>
+        <label>
+          Content: 
+          <textarea 
+            value={content} 
+            onChange={(e)=>setcontent(e.target.value)}
+          />
+        </label>
+
+      </form>
+      {posts?.map((post) => 
+        <div className='post bgcolor'>
+          <header>          
+            <h3>{post.author}</h3>
+            <h1>{post.title}</h1>
+            <div className='stats'>
+              <p>Likes:{post.likes} | Dislikes:{post.dislikes}</p>
+              <p>Posted: {post.posted}</p>
+            </div>
+          </header>
+          {post?.replies?.map((reply)=>
+            <Reply {...reply}/>
+          )}
+        </div>
+      )}
     </div>
   );
+}
+
+
+function Reply(props){
+  return(
+    <div className='post reply'>
+      <header>          
+        <h4>{props.author}</h4>
+        <h2>{props.content}</h2>
+        <div className='stats'>
+          <p>Likes:{props.likes} | Dislikes:{props.dislikes}</p>
+          <p>Posted: {props.posted}</p>
+        </div>
+      </header>
+      {props?.replies.map((reply)=>
+        <Reply {...reply}/>
+      )}
+    </div>
+  )
 }
 
 export default App;
